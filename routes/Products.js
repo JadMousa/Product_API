09ed62d1df0f373057f0978b5842010b3e13d51d
JadMousa <jadmousa12@gmail.com>
2025-05-22 16:30:21 +0300

    handle-update-request

--- routes/Products.js
+++ routes/Products.js
@@ -20,6 +20,26 @@ router.get('/:id', (req, res) => {
       res.status(404).json({ message: "Product not found" });
     }
   });
+
+// PUT update product by ID
+router.put('/:id', (req, res) => {
+    const product = products.find(p => p.id == req.params.id);
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+  
+    // Only update if the value is explicitly sent
+    if (req.body.name !== undefined) {
+      product.name = req.body.name;
+    }
+  
+    if (req.body.price !== undefined) {
+      product.price = req.body.price;
+    }
+  
+    res.json(product);
+  });
+  
   
 
 export default router;
